7e62072b74de7ef4e0346cd0df7f90643f063b94
wh1tepr1nc3 <amamamania@mail.ru>
2024-07-21 22:38:35 +0500

    Add files via upload

---
+++ module1hard.py
@@ -0,0 +1,17 @@
+grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
+students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
+
+students_sort = sorted(students)
+
+# grades_students = [sum(grades[0])/len(grades[0]),sum(grades[1])/len(grades[1]),
+#                   sum(grades[2])/len(grades[2]),sum(grades[3])/len(grades[3]),
+#                   sum(grades[4])/len(grades[4])]
+
+grades_students = []
+
+for i in grades:
+    sl=sum(i)/len(i)
+    grades_students.append(sl)
+
+dict = dict(zip(students_sort, grades_students))
+print(dict)
